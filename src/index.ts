efe9205147bbec7e12f592e9fe56d91bcd781ceb
ahmed <ahmedsz1112@github.com>
2025-06-08 11:16:01 -0700

    add

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,13 @@ import routerCart from "./routers/cartRouter";
 import cors from "cors";
 require("dotenv").config();
 
+
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
+
 
 mongoose
   .connect(process.env.DATABASE_URL || '')
